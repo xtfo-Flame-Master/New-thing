f60b9cec573ad85664281745ab073d9c378bbf52
xtfo-Flame-Master <148436761+xtfo-Flame-Master@users.noreply.github.com>
2024-04-12 13:54:56 +0100

    Update metadata.js

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,13 +1,13 @@
 // update the information in this file with information about you/your site - this is for the search engine to read and display, but isn't shown on your site itself
 
 module.exports = {
-	title: "My Example Blog",
-	url: "https://example.com/",
-	language: "en",
+	title: "I play pokemon GO everyday",
+	url: "https://PokemonCentre.com/",
+	language: "English",
 	description: "I am writing about my experiences as a naval navel-gazer.",
 	author: {
-		name: "Your Name Here",
-		email: "youremailaddress@example.com",
-		url: "https://example.com/about-me/"
+		name: "llor kcir",
+		email:"llorKcir@gmail.com",
+		url: "https://PokemonCentre.com/about-me/"
 	}
 }
